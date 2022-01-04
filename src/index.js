42fcd407e8ce98a40f10b5882bd2cb54718c7452
7ab901a7933419b5 <7ab901a7933419b5@protonmail.com>
2022-01-04 11:15:43 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function toReadable (number) {
-  
+  let ones = [
+    "zero", "one", "two", "three", "four",
+    "five", "six", "seven", "eight", "nine",
+    "ten", "eleven", "twelve", "thirteen", "fourteen",
+    "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"
+  ];
+  let tens = [
+    "twenty", "thirty", "forty", "fifty",
+    "sixty", "seventy", "eighty", "ninety"
+  ];
+  // Assuming the number is between 0 and 999
+  let groups = [];
+  let hundreds = parseInt(number / 100), remainder = number % 100;
+  if (hundreds) { groups.push(`${ones[hundreds]} hundred`); }
+  if (remainder >= 20) {
+    let dozens = parseInt(remainder / 10);
+    if (dozens) { groups.push(tens[dozens-2]); }
+    remainder %= 10;
+  }
+  if (remainder || groups.length == 0) { groups.push(ones[remainder]); }
+  return groups.join(" ");
 }
